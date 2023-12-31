d73c7f71f86df1df81b3da048e19036a11196e8e
CHARAN DASS <43849626+charandass@users.noreply.github.com>
2023-12-31 06:12:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversinOperation.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+
+
+let isLoggedeIn = "fsdsdf"
+
+let booleanIsLoggedIn = Boolean(isLoggedeIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+// "" => false
+// "charan" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
