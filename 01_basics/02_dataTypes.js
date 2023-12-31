6fb56cce602f719f77561890c49e2db504d7b254
CHARAN DASS <43849626+charandass@users.noreply.github.com>
2023-12-31 05:56:58 +0000

    dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs. not browser
+
+console.log(3
+    +3) // code readability should be high
+console.log("hi there")
+
+
+let name = "charn"
+let age = 28
+let isLoggedIn = true
+
+// number => 2 to power 53   range of number
+// bigint
+// string => "" ''
+// boolean => true/false
+// null  => standalone value
+// undefined => 
+// symbol => unique
+
+// oject
+
+console.log(typeof undefined); // undefined
+console.log((typeof null)); // object
